8164d18896ce7f71cea68bd823e40d2b6fe32568
diegoamunoz <diego.amunoz@alumnos.upm.es>
2015-10-29 00:22:02 +0100

    Adapting design to content in engage-for teachers section

--- js/main.js
+++ js/main.js
@@ -126,3 +126,63 @@ function main() {
 
 }
 main();
+
+var openBox = function(id) {
+
+  var box1 = document.getElementById("box-1");
+  var box2 = document.getElementById("box-2");
+  var box3 = document.getElementById("box-3");
+
+  var buttons = document.getElementById("buttons")
+  var images = buttons.getElementsByTagName('img');
+  var titles = buttons.getElementsByTagName('h3');
+  var lines = buttons.getElementsByTagName('hr');
+
+  var sec3 = document.getElementById("sec3");
+
+  if (id == 1) {
+    box1.style.display = "block";
+    box2.style.display = "none";
+    box3.style.display = "none";
+    titles[0].style.color = "#0DB575";
+    titles[1].style.color = "#5A5A5A";
+    titles[2].style.color = "#5A5A5A";
+    images[0].style.opacity = "1";
+    images[1].style.opacity = "0.5";
+    images[2].style.opacity = "0.5";
+    sec3.style.height = "1000px";
+  } else if (id == 2) {
+    box1.style.display = "none";
+    box2.style.display = "block";
+    box3.style.display = "none";
+    titles[0].style.color = "#5A5A5A";
+    titles[1].style.color = "#0DB575";
+    titles[2].style.color = "#5A5A5A";
+    images[0].style.opacity = "0.5";
+    images[1].style.opacity = "1";
+    images[2].style.opacity = "0.5";
+    sec3.style.height = "1000px";
+  } else {
+    box1.style.display = "none";
+    box2.style.display = "none";
+    box3.style.display = "block";
+    titles[0].style.color = "#5A5A5A";
+    titles[1].style.color = "#5A5A5A";
+    titles[2].style.color = "#0DB575";
+    images[0].style.opacity = "0.5";
+    images[1].style.opacity = "0.5";
+    images[2].style.opacity = "1";
+    sec3.style.height = "800px";
+  }
+
+  buttons.style.marginTop = "0";
+  images[0].width = "70";
+  images[1].width = "70";
+  images[2].width = "70";
+  titles[0].style.fontSize = "14px";
+  titles[0].style.marginTop = "0";
+  titles[1].style.fontSize = "14px";
+  titles[1].style.marginTop = "0";
+  titles[2].style.fontSize = "14px";
+  titles[2].style.marginTop = "0";
+}
